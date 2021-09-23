7e8fc38108fb616576c287a809ade2094ad6b082
musekorea <musekorea@hanmail.net>
2021-09-23 16:34:41 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <div className="App"> Hello Moya</div>;
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState((current) => ({ isLoading: false }));
+    }, 5000);
+  }
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        <p>{isLoading ? `Loading...` : `We're ready`}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Moya from './Moya';
-import Test from './Test';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Moya></Moya>
     <App />
-    <Test></Test>
   </React.StrictMode>,
   document.getElementById('root')
 );
